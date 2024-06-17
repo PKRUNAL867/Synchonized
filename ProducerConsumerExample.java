5f5eeac5630edce3da40c6ab9bb9a08a98b36b52
PINKI_PRAJAPATI <prajapatipinki1112@gmail.com>
2024-06-17 09:44:46 +0530

    Add files via upload

---
+++ ProducerConsumerExample.java
@@ -0,0 +1,13 @@
+package interthreadcommunication;
+
+public class ProducerConsumerExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Q q = new Q();
+		new Producer(q);
+		new Consumer(q);
+
+	}
+
+}
